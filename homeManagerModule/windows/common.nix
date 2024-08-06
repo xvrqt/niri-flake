db5c64f42428abcc22643a2b13cf135c8f7bc95b
xvrqt <git@xvrqt.com>
2024-08-05 17:48:02 -0700

    Added Window-Rule to not dim the browser when a YouTube Tab is open

--- homeManagerModule/windows/common.nix
+++ homeManagerModule/windows/common.nix
@@ -7,6 +7,7 @@
 in {
   programs.niri.settings = lib.mkIf cfgCheck {
     window-rules = [
+      # All windows share the same border rounding
       {
         geometry-corner-radius = let
           r = 4.0;
@@ -18,9 +19,18 @@ in {
         };
         clip-to-geometry = true;
       }
+      # Make non-active windows semi-transparent
       {
         matches = [{is-active = false;}];
         opacity = 0.9;
+      }
+      # If the browser is showing a YouTube video, don't make it semi-transparent
+      {
+        matches = [
+          {title = "^.*[Y|y]ou[T|t]ube.*$";}
+          {app-id = "LibreWolf";}
+        ];
+        opacity = 1.0;
       }
     ];
   };
