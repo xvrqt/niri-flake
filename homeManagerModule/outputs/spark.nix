d807daed7c5fa0feec82a9ef31acbeb34db72761
xvrqt <git@xvrqt.com>
2024-11-26 14:33:49 -0800

    test

--- homeManagerModule/outputs/spark.nix
+++ homeManagerModule/outputs/spark.nix
@@ -3,11 +3,11 @@
   config,
   ...
 }: let
-  cfgCheck = config.desktops.niri.enable;
-  monitors = import ./monitors.nix;
+  #cfgCheck = config.desktops.niri.enable;
+  #  monitors = import ./monitors.nix;
   # Get the output name; e.g. "eDP-1"
   OUTPUT = builtins.trace (builtins.head config.desktops.outputs) "eDP-1";
 in {
   #programs.niri.settings.outputs.${OUTPUT} = lib.mkIf cfgCheck monitors.mac-book-pro;
-  programs.niri.settings.outputs.${OUTPUT} = {};
+  programs.niri.settings.outputs.eDP-1 = {};
 }
