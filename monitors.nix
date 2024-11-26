2e76eba7abc8a0d9cdda3c9d2dfde2c0f67ddf72
xvrqt <git@xvrqt.com>
2024-11-26 14:07:39 -0800

    test

---
+++ monitors.nix
@@ -0,0 +1,42 @@
+{
+  # Large Curved Monitor
+  odyssey = {
+    # Probably wrong
+    output = "HDMI-5";
+    mode = {
+      width = 7680;
+      height = 2160;
+      refresh = 119.997;
+    };
+    variable-refresh-rate = true;
+    scale = 1.0;
+    position = {
+      x = 5120;
+      y = 0;
+    };
+    transform = {
+      flipped = false;
+      rotation = 0;
+    };
+  };
+
+  # Mac Book Pro Screen
+  mac-book-pro = {
+    mode = {
+      width = 3024;
+      height = 1890;
+      refresh = 60.0;
+    };
+    variable-refresh-rate = false;
+    scale = 2.0;
+    position = {
+      x = 0;
+      y = 0;
+    };
+    transform = {
+      flipped = false;
+      rotation = 0;
+    };
+    output = "eDP-1";
+  };
+}
