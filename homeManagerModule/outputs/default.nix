397f1862e44768ae3e91b4f24969fd99f6f85a3e
xvrqt <git@xvrqt.com>
2024-11-26 15:08:32 -0800

    test

---
+++ homeManagerModule/outputs/default.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  cfgCheck = config.desktops.niri.enable;
+  # Niri is strict about checking the attrsets passed to parameters
+  # We must filter out the "output" field of the attrset to use it in the config
+  outputToNiriSubModule = output: (lib.attrsets.filterAttrs (n: _: n != "output") output);
+in {
+  programs.niri.settings.outputs =
+    lib.mkIf cfgCheck
+    (builtins.listToAttrs
+      (builtins.map (output: {
+          name = output.output;
+          value = outputToNiriSubModule output;
+        })
+        config.desktops.outputs));
+}
