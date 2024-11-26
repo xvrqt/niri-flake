47b2054107e0f13ef15835710cc2e4ecb80adb45
xvrqt <git@xvrqt.com>
2024-11-26 15:03:30 -0800

    test

--- homeManagerModule/outputs/spark.nix
+++ homeManagerModule/outputs/spark.nix
@@ -4,12 +4,17 @@
   ...
 }: let
   cfgCheck = config.desktops.niri.enable;
-  #  monitors = import ./monitors.nix;
-  # Get the output name; e.g. "eDP-1"
-  OUTPUT = builtins.head config.desktops.outputs;
-  SUBMODULE = lib.attrsets.filterAttrs (n: v: n != "output") OUTPUT;
-  OUTPUT_NAME = OUTPUT.output;
+  outputToNiriSubModule = output: (lib.attrsets.filterAttrs (n: _: n != "output") output);
 in {
-  #programs.niri.settings.outputs.${OUTPUT} = lib.mkIf cfgCheck monitors.mac-book-pro;
-  programs.niri.settings.outputs."${OUTPUT_NAME}" = SUBMODULE;
+  #programs.niri.settings.outputs."${OUTPUT_NAME}" = SUBMODULE;
+  programs.niri.settings.outputs =
+    lib.mkIf cfgCheck # Only make if Niri is enabled
+    
+    (builtins.listToAttrs
+      (builtins.map (output:
+        {
+          name = output.output;
+          value = outputToNiriSubModule output;
+        }
+        config.desktops.outputs)));
 }
