be6b0833ba3a1d7322b28a8b220d6e22a92798f8
xvrqt <git@xvrqt.com>
2024-11-26 14:43:19 -0800

    test

--- flake.nix
+++ flake.nix
@@ -14,49 +14,7 @@
     machines = ["nyaa" "spark"];
   in {
     # My personal monitor collection; as the layout of this is tightly bound, it's nice to have it on hand
-    # monitors = import ./monitors.nix;
-    monitors = {
-      # Large Curved Monitor
-      odyssey = {
-        # Probably wrong
-        output = "HDMI-5";
-        mode = {
-          width = 7680;
-          height = 2160;
-          refresh = 119.997;
-        };
-        variable-refresh-rate = true;
-        scale = 1.0;
-        position = {
-          x = 5120;
-          y = 0;
-        };
-        transform = {
-          flipped = false;
-          rotation = 0;
-        };
-      };
-
-      # Mac Book Pro Screen
-      mac-book-pro = {
-        mode = {
-          width = 3024;
-          height = 1890;
-          refresh = 60.0;
-        };
-        variable-refresh-rate = false;
-        scale = 2.0;
-        position = {
-          x = 0;
-          y = 0;
-        };
-        transform = {
-          flipped = false;
-          rotation = 0;
-        };
-        output = "eDP-1";
-      };
-    };
+    monitors = import ./monitors.nix;
     # You need this regardless if you use the Home Manager Module
     nixosModules = {
       default = {pkgs, ...}: {
--- homeManagerModule/outputs/spark.nix
+++ homeManagerModule/outputs/spark.nix
@@ -7,8 +7,8 @@
   #  monitors = import ./monitors.nix;
   # Get the output name; e.g. "eDP-1"
   #OUTPUT = builtins.trace config.desktops.outputs "eDP-1";
-  OUTPUT = builtins.trace (builtins.head config.desktops.outputs).output "eDP-1";
+  OUTPUT = builtins.head config.desktops.outputs;
 in {
   #programs.niri.settings.outputs.${OUTPUT} = lib.mkIf cfgCheck monitors.mac-book-pro;
-  programs.niri.settings.outputs.${OUTPUT} = {};
+  programs.niri.settings.outputs.${OUTPUT.output} = {};
 }
