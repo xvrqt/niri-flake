c662db4091a0a33276d8a6e4b2f4f62e58d1f9ae
xvrqt <git@xvrqt.com>
2024-11-26 16:15:49 -0800

    test

--- flake.nix
+++ flake.nix
@@ -3,33 +3,45 @@
     niri.url = "github:sodiboo/niri-flake";
     swww.url = "github:LGFae/swww";
     shaderbg.url = "/home/xvrqt/Development/shaderbg-flake";
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     niri,
     swww,
+    nixpkgs,
     shaderbg,
+    flake-utils,
     ...
   }: let
     machines = ["nyaa" "spark"];
+    nixosModulesWrapped = flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      # You need this regardless if you use the Home Manager Module
+      nixosModules = {
+        default = {
+          imports = [
+            # Re-import the original NixOS module from the Niri Flake
+            niri.nixosModules.niri
+            # Import the wallpaper manager NixOS Modules
+            shaderbg.nixosModules.${system}.default
+            # Include our NixOS Module which enables and configures Niri
+            (import ./nixosModule.nix {
+              inherit pkgs niri swww;
+            })
+          ];
+        };
+      };
+    });
   in {
     # My personal monitor collection; as the layout of this is tightly bound, it's nice to have it on hand
     monitors = import ./monitors.nix;
-    # You need this regardless if you use the Home Manager Module
-    nixosModules = {
-      default = {pkgs, ...}: {
-        imports = [
-          # Re-import the original NixOS module from the Niri Flake
-          niri.nixosModules.niri
-          # Import the wallpaper manager NixOS Modules
-          shaderbg.nixosModules.${pkgs.system}.default
-          # Include our NixOS Module which enables and configures Niri
-          (import ./nixosModule.nix {
-            inherit pkgs niri swww;
-          })
-        ];
-      };
-    };
+
+    # NixOS Modules Per System
+    nixosModules = nixosModulesWrapped.nixosModules;
 
     # Home Manager Module is required to declaratively configure Niri
     # (using the Sodiboo Flake, that is)
