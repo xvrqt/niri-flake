326e1578d3cb8ca7effd8480249b8e6700024044
xvrqt <git@xvrqt.com>
2024-12-03 19:21:56 -0800

    test

--- window-managers/niri/nixosModule.nix
+++ window-managers/niri/nixosModule.nix
@@ -1,31 +1,29 @@
 {
-  config,
   lib,
   pkgs,
+  config,
   ...
 }: {
-  config =
-    lib.mkIf config.desktops.window-manager
-    == "niri" {
-      # Enable Niri
-      programs.niri = {
-        enable = true;
-        # Otherwise there is the potential to get out of sync with Niri in NixPkgs
-        package = pkgs.niri-stable;
-      };
-
-      # Enable Wayland support in Electron based applications (gross!)
-      environment.variables.NIXOS_OZONE_WL = "1";
-
-      # Useful packages to include with a Desktop Environment
-      environment.systemPackages = [
-        pkgs.waypaper
-
-        pkgs.cage
-        pkgs.gamescope
-        pkgs.libsecret
-        pkgs.wl-clipboard
-        pkgs.wayland-utils
-      ];
+  config = lib.mkIf (config.desktops.window-manager == "niri") {
+    # Enable Niri
+    programs.niri = {
+      enable = true;
+      # Otherwise there is the potential to get out of sync with Niri in NixPkgs
+      package = pkgs.niri-stable;
     };
+
+    # Enable Wayland support in Electron based applications (gross!)
+    environment.variables.NIXOS_OZONE_WL = "1";
+
+    # Useful packages to include with a Desktop Environment
+    environment.systemPackages = [
+      pkgs.waypaper
+
+      pkgs.cage
+      pkgs.gamescope
+      pkgs.libsecret
+      pkgs.wl-clipboard
+      pkgs.wayland-utils
+    ];
+  };
 }
