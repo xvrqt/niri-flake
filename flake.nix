fa6b263ca357f2fbe931b7e44a316b2169c9882a
xvrqt <git@xvrqt.com>
2024-12-03 19:52:02 -0800

    test

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
             imports = [
               # Annoyingly, Niri requires a NixOS Module to work
               niri.nixosModules.niri
-              (import ./window-managers/niri/nixosModule.nix {
+              (import ./window-managers/nixosModules {
                 inherit lib pkgs niri config;
               })
             ];
