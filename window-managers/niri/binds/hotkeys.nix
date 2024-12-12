c8fe99f0e79f7813860858d9a6a8b6f41b616dcd
xvrqt <git@xvrqt.com>
2024-12-12 09:49:45 -0800

    test

--- window-managers/niri/binds/hotkeys.nix
+++ window-managers/niri/binds/hotkeys.nix
@@ -4,6 +4,7 @@
   ...
 }: let
   # Use the default terminal
+  wallpaperToggle = config.desktops.wallpaper.toggle;
   terminal = config.terminal.emulator or "kitty";
   cfgCheck = config.desktops.window-manager == "niri";
 in {
@@ -16,7 +17,7 @@ in {
         # Open a Terminal
         "Mod+T".action = spawn terminal;
         "Mod+W".action = spawn "librewolf";
-        "Mod+B".action = spawn "shaderbg_toggleWallpaper";
+        "Mod+B".action = spawn wallpaperToggle;
         # Open Rofi Application Launcher
         "Mod+Return".action = sh "rofi -show drun";
         # Show Hotkey Cheat Sheet
