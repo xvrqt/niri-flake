377396adf4bad9bc3af7e462ecaf9d00fc762d29
xvrqt <git@xvrqt.com>
2025-02-03 13:19:17 -0800

    fuck nix

--- window-managers/niri/animations/nyaa.nix
+++ window-managers/niri/animations/nyaa.nix
@@ -9,13 +9,13 @@ in {
   config = lib.mkIf cfgCheck {
     programs.niri.settings = {
       animations = {
-      #   window-close.easing = lib.mkIf config.desktops.niri.animations.window-close.enable {
-      #     duration-ms = 2000;
-      #     curve = "linear";
-      #   };
-      #
-      #   shaders.window-close = lib.mkIf config.desktops.niri.animations.window-close.enable (builtins.toString window_close);
-      # };
+        #   window-close.easing = lib.mkIf config.desktops.niri.animations.window-close.enable {
+        #     duration-ms = 2000;
+        #     curve = "linear";
+        #   };
+        #
+        #   shaders.window-close = lib.mkIf config.desktops.niri.animations.window-close.enable (builtins.toString window_close);
+      };
     };
   };
 }
